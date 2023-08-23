d907a3fd9c20e8fd54eac7a5f00d412489d0a831
trineshk <111257025+trineshk@users.noreply.github.com>
2023-08-23 05:32:53 +0000

    implemented quiz

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+questions = [
+  {
+    "question": "What is the capital of India?",
+    "options" :  ['Delhi', 'Punjab', 'Mumbai', 'Kashmir'],
+    "correct_answer" : 'Delhi'
+    
+  },
+  {
+    "question": "What is the national animal of India?",
+    "options" :  ['Lion', 'Cheetah', 'Tiger', 'Elephant'],
+    "correct_answer" : 'Tiger'
+    
+  },]
+
+
 @app.route("/")
-def helloWorld():
-  return 'Hello Jovian'
+def index():
+  return render_template('quiz.html', questions=questions)
+
+@app.route("/submit", methods=['POST'])
+def submit():
+  score = 0
+  for question in questions:
+    user_answer = request.form.get(question['question'])
+    if user_answer == question['correct_answer']:
+      score += 1
+  return render_template('results.html', score=score, total=len(questions))
+
+
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
+
