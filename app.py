450121d111e30154c1fa1603882c0a94e7a3701b
trineshk <111257025+trineshk@users.noreply.github.com>
2023-08-24 08:47:01 +0000

    home page design, added quiz and rendered results page

--- app.py
+++ app.py
@@ -5,21 +5,31 @@ app = Flask(__name__)
 questions = [
   {
     "question": "What is the capital of India?",
-    "options" :  ['Delhi', 'Punjab', 'Mumbai', 'Kashmir'],
-    "correct_answer" : 'Delhi'
-    
+    "options": ['Delhi', 'Punjab', 'Mumbai', 'Kashmir'],
+    "correct_answer": 'Delhi'
   },
   {
     "question": "What is the national animal of India?",
-    "options" :  ['Lion', 'Cheetah', 'Tiger', 'Elephant'],
-    "correct_answer" : 'Tiger'
-    
-  },]
+    "options": ['Lion', 'Cheetah', 'Tiger', 'Elephant'],
+    "correct_answer": 'Tiger'
+  },
+  {
+    "question": "2 + 5?",
+    "options": [6, 7, 0, 4],
+    "correct_answer": '7'
+  },
+]
 
 
 @app.route("/")
 def index():
+  return render_template('home.html', questions=questions)
+
+
+@app.route("/quiz")
+def quiz():
   return render_template('quiz.html', questions=questions)
+
 
 @app.route("/submit", methods=['POST'])
 def submit():
@@ -28,11 +38,10 @@ def submit():
     user_answer = request.form.get(question['question'])
     if user_answer == question['correct_answer']:
       score += 1
+    print(score)
+
   return render_template('results.html', score=score, total=len(questions))
-
-
 
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
-
